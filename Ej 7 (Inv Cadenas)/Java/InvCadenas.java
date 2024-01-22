0754dd910f7d5ad101f34db78168e085a1bed083
Deathbat0 <danielalejo99@gmail.com>
2024-01-22 16:29:11 -0500

    Actualizado 22/01/2024

---
+++ Ej 7 (Inv Cadenas)/Java/InvCadenas.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class InvCadenas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input;
+
+        System.out.print("Por favor ingrese la cadena a invertir: ");
+        input = sc.nextLine();
+        char cadena[] = new char[input.length()];
+        cadena = input.toCharArray();
+        System.out.println("Cadena invertida: ");
+        for(int i = input.length()-1; i >= 0; i--){
+            System.out.print(cadena[i]);
+        }   
+        sc.close();
+    }
+}
---
+++ Ej 7 (Inv Cadenas)/Python/InvCadenas.py
@@ -0,0 +1,6 @@
+cadena = input("Por favor ingrese la cadena a invertir: ")
+print("Cadena inverida: ")
+k = len(cadena)-1
+while k >= 0:
+    print(cadena[k])
+    k-=1
