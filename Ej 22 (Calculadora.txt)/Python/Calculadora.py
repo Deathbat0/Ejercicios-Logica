201243c3b18c43a8e51e2f7328bb337412c86f69
Deathbat0 <danielalejo99@gmail.com>
2024-02-12 12:45:31 -0500

    Actualizado 12/02/2024

--- Ej 22 (Calculadora.txt)/Python/Calculadora.py
+++ Ej 22 (Calculadora.txt)/Python/Calculadora.py
@@ -3,12 +3,38 @@
 def calcular_resultado(entrada):
 
     try:
-        resultado = eval(entrada)
+        resultado = float(entrada[0].strip())
+        for i in range(1, len(entrada), 2):
+            operador = entrada[i].strip()
+            numero = float(entrada[i + 1].strip())
+            if operador == '+':
+                resultado += numero
+            elif operador == '-':
+                resultado -= numero
+            elif operador == '*':
+                resultado *= numero
+            elif operador == '/':
+                resultado /= numero
         return resultado
-    except:
+    except ZeroDivisionError:
+        print("Error: División por cero.")
         return None
-    
+    except:
+        print("Error: Operación inválida.")
+        return None
 
+operation = []
 file_name = "datos.txt"
-with open(file_name) as archivo:
-    print(archivo.read())
+try:
+    with open(file_name) as archivo:
+        for line in archivo:
+            lin = line.strip()
+            if line:
+                operation.append(float(lin) if lin.replace('.','').isdigit() else lin)
+    
+    print(operation)
+
+except FileNotFoundError:
+    print(f"No se encotro el archivo {file_name}")
+except Exception as e:
+    print("Ocurrio un error", e)
