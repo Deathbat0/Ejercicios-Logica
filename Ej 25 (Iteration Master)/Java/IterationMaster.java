724ea420754854b2427b04aa18945b5610f0f3ae
Deathbat0 <danielalejo99@gmail.com>
2024-02-15 12:50:30 -0500

    Actualizado 15/02/2024

--- Ej 25 (Iteration Master)/Java/IterationMaster.java
+++ Ej 25 (Iteration Master)/Java/IterationMaster.java
@@ -1,11 +1,29 @@
+// DeathbatO
 
 public class IterationMaster {
     public static void main(String[] args) {
         int arr[] = new int[100];
+        int count = 1;
 
         for(int i = 1; i <= 100; i++){
             System.out.println(i);
             arr[i-1] = i;
         }
+
+        for(int i : arr){
+            System.out.println(i);
+        }
+
+        while(count <= 100){
+            System.out.println(count);
+            count++;
+        }
+
+        count = 1;
+        do{
+            System.out.println(count);
+            count++;
+        }while(count <= 100);
+        
     }
 }
