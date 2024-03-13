e63b441e0aeb834f6b620f42ed20d8ba0bffe0f5
Deathbat0 <danielalejo99@gmail.com>
2024-03-13 15:16:42 -0500

    Actualizado 13/03/2024

---
+++ Ej 32 (Bisiestos)/Python/Bisiestos.py
@@ -0,0 +1,8 @@
+count = 0
+year = int(input("Introduce un año: "))
+print("Los proximos 30 años bisiestos son: ")
+while count < 30:
+    year += 1
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        print(year)
+        count += 1
